4c813f47c8c6ec96cb267ba010c6d1ecb5ca5689
ehqkrtlsrl95 <ehqkrtlsrl95@gmail.com>
2020-10-03 20:41:59 +0900

    Initial commit.

--- src/main/java/com/jh/book/springboot/web/IndexController.java
+++ src/main/java/com/jh/book/springboot/web/IndexController.java
@@ -10,22 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+
+//    @GetMapping("/")
+//    public String index(Model model, @LoginUser SessionUser user) {
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        if (user != null) {
+//            model.addAttribute("userName", user.getName());
+//        }
+//        return "index";
+//    }
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("posts", postsService.findAllDesc());
-
-        if(user != null) {
-            model.addAttribute("userName", user.getName());
-        }
+    public String index() {
         return "index";
     }
 
@@ -41,5 +42,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
 }
